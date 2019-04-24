8e7ce7df64ce947e09f03406b37ef24579654e9a
Oussama Bougaila <oussama.bougaila@gmail.com>
2019-04-24 13:51:56 +0100

    feat: Add filtered news

--- app/src/main/java/com/example/bougy/dz_now/FilteredListActivity.kt
+++ app/src/main/java/com/example/bougy/dz_now/FilteredListActivity.kt
@@ -1,74 +1,25 @@
 package com.example.bougy.dz_now
 
-import android.content.Intent
-import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.view.Menu
-import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_actuality_detail.*
+import kotlinx.android.synthetic.main.activity_filtered_list.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class FilteredListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setContentView(R.layout.activity_filtered_list)
 
+        val categoryTitle = intent.getStringExtra("title")
+        setTitle(categoryTitle)
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
-
-        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        recyclerView_filtered.layoutManager = LinearLayoutManager(this)
 
         fetchJson()
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        val k = item.title
-        val intent = Intent(this, FilteredListActivity::class.java)
-        intent.putExtra("title", item.title)
-        startActivity(intent)
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
     }
 
     fun fetchJson() {
@@ -100,17 +51,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 "}" +
                 "]" +
                 "}"
-        println(body)
         val gson = GsonBuilder().create()
 
-        val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+        val homeFeedAll = gson.fromJson(body, HomeFeed::class.java)
+        val categoryTitle = intent.getStringExtra("title")
+        var actualities: List<Actuality> = homeFeedAll.actualities.filter { it.theme == categoryTitle }
+
+        var homeFeed = HomeFeed(actualities)
         runOnUiThread {
-            recyclerView_main.adapter = MainAdapter(homeFeed)
+            recyclerView_filtered.adapter = MainAdapter(homeFeed)
         }
 
     }
 }
-
-class HomeFeed(val actualities: List<Actuality>)
-
-class Actuality(val id: Int, val title: String, val description: String, val main_image: String, val time: String, val theme: String)
