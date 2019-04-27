0aeec3f73c7e5e7a23647162ab96fb8c2023f88d
Oussama Bougaila <oussama.bougaila@gmail.com>
2019-04-27 08:10:42 +0100

    feat: Add share feature

--- app/src/main/java/com/example/bougy/dz_now/ActualityDetailActivity.kt
+++ app/src/main/java/com/example/bougy/dz_now/ActualityDetailActivity.kt
@@ -1,7 +1,10 @@
 package com.example.bougy.dz_now
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_actuality_detail.*
@@ -47,6 +50,30 @@ class ActualityDetailActivity : AppCompatActivity() {
             prefs.actualities = body
 
 
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.share, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        var title = intent.getStringExtra("actualityTitle")
+        var description = intent.getStringExtra("actualityDescription")
+        when (item.itemId) {
+            R.id.action_share -> {
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, title+": "+description)
+                intent.type = "text/plain"
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
